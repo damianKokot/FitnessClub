61d3e943218f00bf1346f6007bf9df09fd326ff1
Szymon Skoczylas <skoczylas9szymon@gmail.com>
2020-01-02 16:28:42 +0100

    Modyfied mydata.js

--- src/controllers/api/mydata.js
+++ src/controllers/api/mydata.js
@@ -5,9 +5,11 @@ router.get('/', function(req, res, next){
 	if(!req.headers['x-auth']){
 		return res.sendStatus(401);
 	}
-	const auth = jwt.decode(req.headers['x-auth'], config.secret)
+	const auth = router.decode(req.headers['x-auth'], config.secret)
 	User.getUserValues(['firstname', 'lastname', 'email', 'telephone'], auth.email, function(err, user){
 		if(err) { return next(err); }
 		res.json(user);
 	});
 });
+
+module.exports = router;
